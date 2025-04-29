a9a3e751572fd5559102977f0d1e2cf080c68226
theRADfarmer <theradfarmer@gmail.com>
2025-04-29 18:39:09 -0500

    Setup initial models and Db.
    - Created `Assignment.cs` and `Subject.cs` models.
    - Set up EF Core
    - Set up development Db and added initial migration

---
+++ HomeschoolGradeTracker.Domain/Entities/Assignment.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeschoolGradeTracker.Domain.Entities
+{
+    public class Assignment
+    {
+        [Key]
+        public int Id { get; set; }
+        public int SubjectId { get; set; }
+        public Subject Subject { get; set; } = default!;
+        [Required]
+        public string AssignmentName { get; set; } = string.Empty;
+        public DateTime DateCompleted { get; set; }
+        public double Grade { get; set; }
+    }
+}
---
+++ HomeschoolGradeTracker.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using HomeschoolGradeTracker.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeschoolGradeTracker.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Subject> Subjects => Set<Subject>();
+        public DbSet<Assignment> Assignments => Set<Assignment>();
+    }
+}
